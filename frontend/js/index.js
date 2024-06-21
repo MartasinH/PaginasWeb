7b5350ab942d5e03e41b60e05c800999f2832f2f
Marta <lugom15122@gmail.com>
2024-06-21 15:13:48 -0600

    Add files via upload

---
+++ frontend/js/index.js
@@ -0,0 +1,14 @@
+
+var colores = ['rojo', 'azul', 'verde'];
+console.log(Ecolores);
+colores.push('negro', 'blanco');
+console.log(colores);
+//Interacción con HTML
+var opciones = '';
+var tabla = '';
+for (var i = 0; i < colores, length; i++) {
+    opciones += '<coption value="' + i + '">' + colores[i] + '</option>';
+    tabla += '<tr><td>' + (i + 1) + '</td><td>' + colores[i] + '</td></tr>';
+}
+document.queryselector('#combo1').innerHTML = opciones;
+document.querySelector('#body').innerHTML = tabla;
